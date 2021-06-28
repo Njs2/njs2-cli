6c5eb488c63d761c449295aaf289bfb726de471a
gautham-juego <gautham.rao@juegostudioz.com>
2021-06-28 14:14:49 +0530

    Updated create project base git URL

--- helper/new-project.js
+++ helper/new-project.js
@@ -2,6 +2,7 @@
 const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const BASE_PACKAGE_URL = 'https://github.com/Njs2/njs2-base.git';
 
 const createProject = async (CLI_KEYS, CLI_ARGS) => {
   if (CLI_ARGS.length == 0) {
@@ -17,7 +18,9 @@ const createProject = async (CLI_KEYS, CLI_ARGS) => {
     let packageJson = require(`${path.resolve(process.cwd(), `${PROJECT_NAME}/package.json`)}`);
     packageJson['njs2-type'] = 'project';
     fs.writeFileSync(`${path.resolve(process.cwd(), `${PROJECT_NAME}/package.json`)}`, JSON.stringify(packageJson, null, 2));
-    child_process.execSync(`cd ${PROJECT_NAME} && njs2 plugin njs2-base`, { stdio: 'inherit' });
+    child_process.execSync(`cd ${PROJECT_NAME} && npm i ${BASE_PACKAGE_URL}`, { stdio: 'inherit' });
+    child_process.execSync(`cd ${PROJECT_NAME} && cp -rn ./node_modules/njs2-base/package/template/frameworkStructure/. .`, { stdio: 'inherit' });
+    child_process.execSync(`cd ${PROJECT_NAME} && npm i -D serverless-prune-plugin serverless-offline`, { stdio: 'inherit' });
   } catch (e) {
     console.log(e);
   }
