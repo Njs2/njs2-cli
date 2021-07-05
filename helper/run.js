0c66b7d6e9dc894da54e84c558bfafa402481880
gautham-juego <gautham.rao@juegostudioz.com>
2021-07-05 13:58:22 +0530

    Added cms run comand

--- helper/run.js
+++ helper/run.js
@@ -2,7 +2,10 @@ const child_process = require("child_process");
 
 const run = async (CLI_KEYS, CLI_ARGS) => {
   await require('./update-postman').updatePostman();
-  if(CLI_ARGS[0] == 'serverless') {
+  if (!Object.keys(CLI_KEYS).includes('skip-cms-build'))
+    child_process.execSync(`cd cms && npm run build`, { stdio: 'inherit' });
+
+  if (CLI_ARGS[0] == 'serverless') {
     child_process.execSync('sls offline start', { stdio: 'inherit' });
   } else {
     child_process.execSync('node express.js', { stdio: 'inherit' });
