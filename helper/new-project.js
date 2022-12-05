0985d08742242f9ae841240dee79d6c3ac856153
Ganesh S Acharya <ganesh.a@juegostudio.com>
2022-12-05 14:52:51 +0530

    njs2 project command can take @njs2/base version value

--- helper/new-project.js
+++ helper/new-project.js
@@ -2,12 +2,12 @@
 const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
-// TODO: @njs2/base
-const BASE_PACKAGE_URL = 'https://github.com/Njs2/njs2-base.git#main';
+const { checkAndFindVersion } = require('./utils');
 
 const execute = async (CLI_KEYS, CLI_ARGS) => {
   try {
     const PROJECT_NAME = CLI_ARGS[0];
+    let BASE_VERSION = "latest"
     // Validations
     if (!PROJECT_NAME) {
       throw new Error('Project name is mandatory parameter');
@@ -17,8 +17,15 @@ const execute = async (CLI_KEYS, CLI_ARGS) => {
       throw new Error(`Project folder already exists: ${PROJECT_NAME}`);
     }
 
+    if (checkAndFindVersion(CLI_ARGS)) {
+      BASE_VERSION = checkAndFindVersion(CLI_ARGS)
+    } else {
+      console.log("Defaulting to @njs2/base@latest")
+      console.log("Did you know now you can pass version to njs2 project command? e.g.: njs2 project <project-name> version 2.0.1")
+    }
+
     child_process.execSync(`mkdir ${PROJECT_NAME} && cd ${PROJECT_NAME} && npm init -y`, { stdio: 'inherit' });
-    child_process.execSync(`cd ${PROJECT_NAME} && npm i ${BASE_PACKAGE_URL}`, { stdio: 'inherit' });
+    child_process.execSync(`cd ${PROJECT_NAME} && npm i @njs2/base@${BASE_VERSION}`, { stdio: 'inherit' });
     const dependencies = require(`${path.resolve(process.cwd(), `${PROJECT_NAME}/package.json`)}`).dependencies;
     child_process.execSync(`cd ${PROJECT_NAME} && cp -rf ./node_modules/@njs2/base/template/frameworkStructure/. .`, { stdio: 'inherit' });
     let packageJson = JSON.parse(fs.readFileSync(`${path.resolve(process.cwd(), `${PROJECT_NAME}/package.json`)}`, 'utf8'));
